15a73a36508be0197159312b5496605e8f1bae74
Juan Antonio Jaramillo Ramirez <juan.jaramillo@DATALYTICS.local>
2025-02-18 08:38:45 -0500

    Se agrega el utils con las preguntas y respuestas

---
+++ utils/utils.py
@@ -0,0 +1,26 @@
+text = """
+{
+¿Qué es un proyecto de interconexión eléctrica (o de transmisión de energía)?:
+Es la construcción de un conjunto de estructuras – torres, líneas y subestaciones- donde se recibe, transforma y transporta la energía eléctrica a alto voltaje que llega de las generadoras ubicadas en diferentes zonas del país y que debe pasar por subestaciones “elevadoras”o “reductoras” que establecen los niveles de tensión adecuados para la transmisión o transporte y la distribución, hasta llegar finalmente a los hogares e industrias, tras recorrer la diversa geografía del país. 
+¿Qué es una línea de transmisión?:
+Las líneas de transmisión que hacen parte de un proyecto de interconexión, son un conjunto de estructuras conformadas por torres, cables y aisladores, que permiten transportar energía de un punto geográfico a otro. Son la manera más económica de mover enormes cantidades de energía y utilizar altos voltajes mediante cables o líneas conductoras de gran calibre, sostenidas por torres o estructuras especiales aisladas de los cables. 
+¿Dónde se genera la energía que circulará por las líneas de transmisión?:
+La generación es el proceso de convertir otras fuentes de energía en energía eléctrica. En Colombia se produce en centrales hidroeléctricas o termoeléctricas tales como Urrá, Ituango y Cerro Matoso, y llega a subestaciones desde donde se incorpora al Sistema Interconectado Nacional (SIN).
+¿Qué es el Sistema Interconectado Nacional (SIN) de energía?:
+Según la Comisión de Regulación de Energía y Gas -CREG-, es el sistema compuesto por los siguientes elementos conectados entre sí: las plantas y equipos de generación, la red de interconexión, las redes regionales e interregionales de transmisión, las redes de distribución y las cargas eléctricas de los usuarios, conforme a lo definido en la Ley 143 de 1994. 
+¿Quién es el responsable de estas líneas de transmisión?:
+INTERCOLOMBIA, empresa de ISA, dedicada al transporte de energía eléctrica a alto voltaje. Es una empresa de servicios mixta, constituida como sociedad anónima, encargada de desarrollar, administrar, operar y mantener los activos de ISA en el Sistema Interconectado Nacional. 
+Nuestra empresa es el mayor transportador de energía en el país y el único con cubrimiento nacional. Sus redes de transporte de energía se extienden a través de la diversa geografía nacional, aportando al desarrollo y a la competitividad de los colombianos. 
+¿Cuál es el papel de la Unidad de Planeación Minero Energética (UPME) en el Sistema Eléctrico Nacional?:
+La UPME es una Unidad Administrativa Especial adscrita al Ministerio de Minas y Energía. Sus funciones son asesorar al Ministerio en la formulación de políticas para los sectores de minas y energía y brindarle información necesaria para tomar decisiones. La UPME formula los planes necesarios para promover el adecuado aprovechamiento de los recursos mineros y para garantizar el óptimo y oportuno abastecimiento de los recursos energéticos.
+¿Cuál es el papel del Ministerio de Minas y Energía en el Sistema Eléctrico Nacional?:
+El Ministerio de Minas y Energía es la entidad del gobierno nacional encargada de administrar los recursos naturales no renovables del país para asegurar que se utilicen de la mejor manera. Este Ministerio se encarga de formular, adoptar, dirigir y coordinar dos políticas que tienen que ver con el Sistema Eléctrico Nacional. La primera es la de generación, transmisión, distribución y comercialización de energía eléctrica. Y la segunda es sobre el uso racional de energía y el desarrollo de fuentes alternas de energía. Además, este Ministerio debe promover, organizar y asegurar el desarrollo de los programas de uso racional y eficiente de energía.
+¿Qué es el Sistema de Transmisión Regional (STR)?:
+Según la Comisión de Regulación de Energía y Gas (CREG), es el sistema interconectado de transmisión de energía eléctrica compuesto por redes regionales o interregionales de transmisión. Se conforma por el conjunto de líneas y subestaciones, con sus equipos asociados, que operan a tensiones menores de 220 mil voltios y que no pertenecen a un sistema de distribución local.
+¿Qué es el Sistema de Distribución Local (SDL)?:
+Según la Comisión de Regulación de Energía y Gas (CREG), es el sistema de transmisión de energía eléctrica compuesto por redes de distribución municipales o distritales. Se conforma por el conjunto de líneas y subestaciones, con sus equipos asociados, que operan a tensiones menores de 220 mil voltios. Estas líneas no pertenecen a un sistema de transmisión regional por estar dedicadas al servicio de un sistema de distribución municipal, distrital o local.
+¿Qué es el Sistema Eléctrico Nacional?:
+El Sistema Eléctrico Nacional está conformado por todos los actores que participan en la cadena de energía (generación, transmisión, distribución y comercialización) y por las autoridades encargadas de planear, regular y vigilar el sector eléctrico. 
+
+}
+"""
