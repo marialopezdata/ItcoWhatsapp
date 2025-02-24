c1902d093d5bbfacb18b890e55e5d7b307c8dd94
Juan Antonio Jaramillo Ramirez <juan.jaramillo@DATALYTICS.local>
2025-02-24 15:26:41 -0500

    se agrega creación de json y envio a cosmos

--- HttpTriggerFunction/functionHttp.py
+++ HttpTriggerFunction/functionHttp.py
@@ -3,7 +3,13 @@ import os
 import azure.functions as func
 import json
 import requests
+import io
 from langchain.chat_models import AzureChatOpenAI
+from datetime import datetime
+from pytz import timezone
+from azure.cosmos import CosmosClient
+
+
 
 from langchain.schema import (
     AIMessage,
@@ -78,3 +84,77 @@ def send_whatsapp_message(body, message):
         logging.error(f"Error al enviar mensaje de WhatsApp: {e}")
     except Exception as e:
         logging.error(f"Error inesperado en send_whatsapp_message: {e}")
+
+
+
+# Función para almacenar los mensajes entrantes y salientes (webhooks)
+def create_json(message,content,created,message_body_response):
+    try:        
+        # Adicionar fecha de recepción del mensaje       
+        receipt_date = datetime.fromtimestamp(int(message["timestamp"]))
+        receipt_date = receipt_date.astimezone(timezone('America/Bogota'))
+        receipt_date = receipt_date.strftime('%d-%m-%Y %H:%M')
+        add_receipt_date = {"receipt_date": receipt_date}
+        message.update(add_receipt_date)
+
+        # Adicionar fecha de respuesta del mensaje
+        response_date = datetime.fromtimestamp(int(created))
+        logging.info (f"response_date: {response_date}")
+        response_date = response_date.astimezone(timezone('America/Bogota'))
+        logging.info (f"response_date2: {response_date}")
+        response_date = response_date.strftime('%d-%m-%Y %H:%M')
+        logging.info (f"response_date3: {response_date}")
+        add_response_date = {"response_date": response_date}
+        message.update(add_response_date)
+        logging.info ("Entra fecha de respuesta del mensaje")
+
+        # Si el blob no existe, crear uno nuevo y almacenar el mensaje inicial
+        with io.StringIO() as file_body:
+            if message["type"] == "audio":
+                add_transcript = {"transcript": message_body_response}
+                logging.info (add_transcript)
+                message.update(add_transcript)
+            initial_data = {
+                "message_from_user": message,
+                "message_to_user": {"text": content}                    
+            }
+            json.dump(initial_data, file_body, indent=4)
+            # file_body.seek(0)
+            # blob_client.upload_blob(file_body.read())
+            save_to_cosmos_db(message, content)
+
+    except Exception as e:
+        logging.error(f'ERROR - save the Webhook: {e}')
+        raise
+
+
+# Función para guardar JSON en Cosmos DB con claves de partición
+def save_to_cosmos_db(message, response):
+    # Conexión a Cosmos DB
+    db_endpoint = os.environ["COSMOS_ENDPOINT"]
+    db_key = os.environ["COSMOS_KEY"]
+    db_name = os.environ["COSMOS_DATABASE_NAME"]
+    db_container = os.environ["COSMOS_CONTAINER_NAME"]
+
+    try:        
+        # Obtiene la referencia a la base de datos y al contenedor
+        client = CosmosClient(db_endpoint, db_key)
+        database = client.get_database_client(db_name)
+        container = database.get_container_client(db_container)
+                
+        # Crea un documento con el mensaje y la respuesta
+        document = {
+            "id": message["id"],
+            "from": message["from"],
+            "message_from_user": message,
+            "message_to_user": {"text": response},
+        }
+                
+        # Inserta el documento en el contenedor de Cosmos DB
+        container.create_item(document)
+        
+        logging.info(f"Guardado en Cosmos DB: {message['id']}")
+        
+    except Exception as e:
+        logging.error(f"ERROR - Conexión a Cosmos DB: {e}")
+        raise
